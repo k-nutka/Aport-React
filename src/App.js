a916777721b8e55e47f08db0402cafed8f5789f9
k-nutka <knutka96@gmail.com>
2023-10-29 16:30:29 +0100

    Dogs aplication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import mainDog from "./images/mainDog.png";
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header-content">
+          <img src={mainDog} className="header-content__img" alt="dog" />
+          <p class="header-content__title">Aport</p>
+          <p class="header-content__text">
+            Choose a dog and learn more about it
+          </p>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
